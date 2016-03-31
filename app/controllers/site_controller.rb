acad6ff9bd34c10e136a291e28658cef65a2835e
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-03-31 19:02:37 +1300

    Add styling to fonts and glyphicons

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,13 +2,15 @@ class SiteController < ApplicationController
 
   def index
     @fonts = Font.all
+    session[:voting_id] = request.remote_ip
+    @voter = Session.find_or_create_by(ip: session[:voting_id])
   end
 
   def upvote
     @font = Font.find(params[:id])
     session[:voting_id] = request.remote_ip
-    voter = Session.find_or_create_by(ip: session[:voting_id])
-    voter.likes @font
+    @voter = Session.find_or_create_by(ip: session[:voting_id])
+    @voter.likes @font
     respond_to do |format|
       format.html { redirect_to :back }
       format.js
