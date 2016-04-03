0e675d76d2639767f692ef0b141e1eacd2802f60
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-04-03 19:01:28 +1200

    font models asc for heroku

--- app/models/font.rb
+++ app/models/font.rb
@@ -2,11 +2,11 @@ class Font < ActiveRecord::Base
   acts_as_votable
 
   def self.vote_order
-    Font.order(cached_votes_up: :desc)
+    Font.order(cached_votes_up: :asc)
   end
 
   def self.created_at
-    Font.order(created_at: :asc)
+    Font.order(created_at: :desc)
   end
 
 end
